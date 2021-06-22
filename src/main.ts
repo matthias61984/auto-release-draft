108a155cd6de0ab560d759d1d44735939a0376e6
CoreyWalsh <matthias61984@hotmail.com>
2021-06-22 14:14:58 -0700

    Add CLI .ts files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
+import * as git from './git'
 
 async function run(): Promise<void> {
   try {
+    const tag = event.getCreatedTag()
+
+    if (tag && version.isSemVer(tag)) {
+      const changelog = await git.getChangesIntroducedByTag(tag)
+    }
+
     core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     core.setFailed(error.message)
